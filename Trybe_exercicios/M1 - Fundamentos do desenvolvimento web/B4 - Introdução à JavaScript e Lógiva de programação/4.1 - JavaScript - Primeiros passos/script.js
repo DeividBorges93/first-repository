361db832d9bce58b93bb6db7ab863f239b572b73
Deivid <deividmborges@gmail.com>
2021-07-28 20:41:04 -0300

    adiciona exercicio 9

--- Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B4 - Introdução à JavaScript e Lógiva de programação/4.1 - JavaScript - Primeiros passos/script.js
+++ Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B4 - Introdução à JavaScript e Lógiva de programação/4.1 - JavaScript - Primeiros passos/script.js
@@ -121,11 +121,24 @@
 
                       // EXERCÍCIO 8
                       
-let num1 = 5;
-let num2 = 9;
-let num3 = 7;
+// let num1 = 5;
+// let num2 = 9;
+// let num3 = 7;
 
-if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+// if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+
+                      // EXERCÍCIO 8
+
+let num1 = 2;
+let num2 = 6;
+let num3 = 8;
+
+if (num1 % 2 === 1 || num2 % 2 === 1 || num3 % 2 === 1) {
     console.log(true)
 }
 else {
