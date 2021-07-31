7b72e5c37b7b469ea162804ce02da4335b35a7ea
Deivid <deividmborges@gmail.com>
2021-07-31 15:02:22 -0300

    adiciona exercicio 1 e 2.

---
+++ Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B4 - Introdução à JavaScript e Lógiva de programação/4.3 - JavasScript - Logica de programaçao e algoritmos/script.js
@@ -0,0 +1,37 @@
+// Exercício 1
+
+// let n = 5;
+
+// for (let i = 1; i <= n; i += 1) {
+//     let str = '';
+//     for (let i =1; i <= n; i += 1) {
+//         str += '*'
+//     }
+//     console.log(str);
+// }
+
+
+// // Exercício 2
+
+
+// let n = 5;
+// let tamanho = "";
+
+// for(let i = 1; i <= n; i += 1) {
+//     tamanho += "*";
+//     console.log(tamanho);
+// }
+
+// // Exercício 3
+
+
+let n = 5;
+let coluna = [0];
+let linha = [0];
+
+
+for(let i = 1; i <= n; i += 1) {
+    if ();
+    console.log(tamanho);
+    
+}
