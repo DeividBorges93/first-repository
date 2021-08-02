f0d7d227ccd01aa85c2cf9b7e60c485e9845870e
Deivid <deividmborges@gmail.com>
2021-08-02 19:09:47 -0300

    adc exercicio

--- Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B4 - Introdução à JavaScript e Lógiva de programação/4.3 - JavasScript - Logica de programaçao e algoritmos/script.js
+++ Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B4 - Introdução à JavaScript e Lógiva de programação/4.3 - JavasScript - Logica de programaçao e algoritmos/script.js
@@ -1,37 +1,37 @@
 // Exercício 1
 
-// let n = 5;
+let n = 5;
 
-// for (let i = 1; i <= n; i += 1) {
-//     let str = '';
-//     for (let i =1; i <= n; i += 1) {
-//         str += '*'
-//     }
-//     console.log(str);
-// }
+for (let i = 1; i <= n; i += 1) {
+    let str = '';
+    for (let i =1; i <= n; i += 1) {
+        str += '*'
+    }
+    console.log(str);
+}
 
 
 // // Exercício 2
 
 
-// let n = 5;
-// let tamanho = "";
+let n = 5;
+let tamanho = "";
 
-// for(let i = 1; i <= n; i += 1) {
-//     tamanho += "*";
-//     console.log(tamanho);
-// }
+for(let i = 1; i <= n; i += 1) {
+    tamanho += "*";
+    console.log(tamanho);
+}
 
 // // Exercício 3
 
 
 let n = 5;
-let coluna = [0];
-let linha = [0];
-
+let tamanho = " ";
+let linha;
 
 for(let i = 1; i <= n; i += 1) {
-    if ();
+    tamanho = n - i;
+    linha = tamanho (n - i);
     console.log(tamanho);
-    
 }
+console.log('Deivid'.slice(0,-2))
