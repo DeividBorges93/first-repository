48b40a6b9a2829c534316343c2a1e598d77b9dd9
Deivid <deividmborges@gmail.com>
2021-08-02 19:12:33 -0300

    adiciona exercicio 5

--- Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B4 - Introdução à JavaScript e Lógiva de programação/4.4 - JavaScript - Objetos e Funçoes/script.js
+++ Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B4 - Introdução à JavaScript e Lógiva de programação/4.4 - JavaScript - Objetos e Funçoes/script.js
@@ -25,4 +25,24 @@ for(let key in info) {
     console.log(info[key]);
  }
  
+ // 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+ let info2 = {
+    personagem: 'Tio patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  };
+ 
+  console.log(info.personagem + ' e ' + info2.personagem);
+  console.log(info.origem + ' e ' + info2.origem);
+  console.log(info.nota + ' e ' + info2.nota);
+  if (info.recorrente === 'Sim' && info2.recorrente === 'Sim') {
+      console.log('Ambos recorrentes');
+  }
+  else if(info.recorrente === 'Sim' || info2.recorrente === 'Sim') {
+      console.log('Apenas um recorrente');
+  }
+  else {
+      console.log('Não recorrentes');
+  }
  
