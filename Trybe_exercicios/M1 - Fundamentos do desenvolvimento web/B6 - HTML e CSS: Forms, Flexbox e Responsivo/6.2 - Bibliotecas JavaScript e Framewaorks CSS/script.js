d2a66cc4ee75c87649b59463af53ca9177db316c
Deivid <deividmborges@gmail.com>
2021-08-20 10:58:09 -0300

    adiciona validaçoes pelo just validate

--- Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B6 - HTML e CSS: Forms, Flexbox e Responsivo/6.2 - Bibliotecas JavaScript e Framewaorks CSS/script.js
+++ Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B6 - HTML e CSS: Forms, Flexbox e Responsivo/6.2 - Bibliotecas JavaScript e Framewaorks CSS/script.js
@@ -1,45 +1,54 @@
+$(document).ready(function(){
+  $('select').formSelect();
+});
+
+document.getElementById('date').DatePickerX.init();
+
+new window.JustValidate('.js-form', {
+  rules: {
+    cpf: {
+      required: true
+    },
+    endereço: {
+      required: true
+    },
+    cidade: {
+      required: true
+    },
+    resumesummary: {
+      required: true
+    },
+    office: {
+      required: true
+    },
+    officedescription: {
+      required: true
+    },
+  },  
+  messages: {
+    name:'Digite seu nome corretamente.',
+    email:'Digite seu e-mail corretamente.',
+    cpf:'Digite seu CPF corretamente',
+    endereço:'Digite seu endereço corretamente',
+    cidade:'Digite sua cidade corretamente',
+    resumesummary:'Resuma seu currículo',
+    office:'Digite seu cargo corretamente',
+    officedescription:'Descreva sobre seu cargo.',
+  },
+});
+
+
 const estadosBrasileiros = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal']
 const selectEstados = document.getElementById('estado');
 function createOptions() {
   for (let index = 0; index < estadosBrasileiros.length; index += 1) {
     const itemOption = document.createElement('option');
+    // itemOption.classList.add('validate');
     selectEstados.appendChild(itemOption).innerText = estadosBrasileiros[index];
     selectEstados.appendChild(itemOption).value = estadosBrasileiros[index];
   }
 }
 createOptions();
-
-function checkDate() {
-  const date = document.getElementById('date');
-  let day = date.value.substring(0, 2);
-  let month = date.value.substring(3, 5);
-  let year = date.value.substring(6, 10);
- 
-  if (day < 0 || day > 31) {
-    alert('Dia inválido!');
-  }
-  if (month < 0 || month > 12) {
-    alert('Mês inválido!');
-  }
-  if (year < 0) {
-    alert('Ano inválido');
-  }
-}
-const inputSubmit = document.getElementById('submit-input');
-function buttonCheckDate() {
-  inputSubmit.addEventListener('click', checkDate);
-}
-buttonCheckDate();
-
-function preventDefault(event) {
-  event.preventDefault();
-  getValues();
-}
-function buttonPreventDefault() {
-  const activePreventDefault = document.getElementById('submit-input');
-  activePreventDefault.addEventListener('click', preventDefault); 
-}
-buttonPreventDefault();
 
 function getValues() {
   const backupDiv = document.getElementById('backup');
@@ -50,10 +59,4 @@ function getValues() {
     paragraph.innerHTML = newValue;
     backupDiv.appendChild(paragraph);
   }
-}
-window.onload = initPage;
-
-function initPage() {
-  
-  
 }
