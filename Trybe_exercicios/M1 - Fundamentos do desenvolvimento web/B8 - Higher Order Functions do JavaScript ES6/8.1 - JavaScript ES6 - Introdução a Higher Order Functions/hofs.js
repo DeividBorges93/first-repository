2b8a404db047a7fc2ceeedcc2fdd0cf05fbad756
Deivid <deividmborges@gmail.com>
2021-09-02 17:47:43 -0300

    Adiciona exercício 1

--- Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B8 - Higher Order Functions do JavaScript ES6/8.1 - JavaScript ES6 - Introdução a Higher Order Functions/hofs.js
+++ Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B8 - Higher Order Functions do JavaScript ES6/8.1 - JavaScript ES6 - Introdução a Higher Order Functions/hofs.js
@@ -1,13 +1,15 @@
-const newPerson = (obj) =>
+const newPerson = (nome) => {
+  return {
+    name: nome, 
+    email: `${nome.toLowerCase().split(' ').join('_')}@trybe.com`};
+};
 
-
-
-
-const newEmployees = () => {
+const newEmployees = (callback) => {
   const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
+console.log(newEmployees(newPerson));
