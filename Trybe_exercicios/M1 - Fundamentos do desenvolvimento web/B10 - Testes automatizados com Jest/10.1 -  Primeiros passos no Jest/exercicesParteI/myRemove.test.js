bc192bc59b103aad3dbf845555ab7a952d0cf1b6
Deivid <deividmborges@gmail.com>
2021-09-21 19:04:14 -0300

    adiciona requisitos exercicio 2 mas nao conclui

--- Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B10 - Testes automatizados com Jest/10.1 -  Primeiros passos no Jest/exercicesParteI/myRemove.test.js
+++ Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B10 - Testes automatizados com Jest/10.1 -  Primeiros passos no Jest/exercicesParteI/myRemove.test.js
@@ -1,2 +1,9 @@
 const myRemove = require('./myRemove');
 
+test('verifica se passado "myRemove([1, 2, 3, 4], 3)" remove o 3 do array', () => {
+  expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+});
+test('verifica se a chamada "myRemove([1, 2, 3, 4], 3)" não retorna o array [1, 2, 3, 4]', () => {
+  expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+});
+test('')
