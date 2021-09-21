edfb2fb4fe6dd7abb74f90745f7d4cf4d94530d1
Deivid <deividmborges@gmail.com>
2021-09-21 20:02:35 -0300

    Adiciona restante da resolução do exercício 2

--- Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B10 - Testes automatizados com Jest/10.1 -  Primeiros passos no Jest/exercicesParteI/myRemove.test.js
+++ Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B10 - Testes automatizados com Jest/10.1 -  Primeiros passos no Jest/exercicesParteI/myRemove.test.js
@@ -6,4 +6,11 @@ test('verifica se passado "myRemove([1, 2, 3, 4], 3)" remove o 3 do array', () =
 test('verifica se a chamada "myRemove([1, 2, 3, 4], 3)" não retorna o array [1, 2, 3, 4]', () => {
   expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
 });
-test('')
+test('verifica se o array passado por parâmetro não sofreu alterações', () => {
+  const myArray = [1, 2, 3, 4];
+  myRemove(myArray);
+  expect(myArray).toEqual([1, 2, 3, 4]);
+});
+test('verifica se a chamada "myRemove([1, 2, 3, 4], 5)" retorna o array esperado', () => {
+  expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+})
