d87ef58e20d9253a4f2b5c6c7842eb6baa665867
Deivid <deividmborges@gmail.com>
2021-09-22 18:50:03 -0300

    Adiciona resolução do exercício 1

--- Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B10 - Testes automatizados com Jest/10.2 - Jest - Testes Assíncronos/exercises/upperCase.test.js
+++ Trybe_exercicios/M1 - Fundamentos do desenvolvimento web/B10 - Testes automatizados com Jest/10.2 - Jest - Testes Assíncronos/exercises/upperCase.test.js
@@ -1,2 +1,12 @@
-const upperCase = require('./upperCase');
+const uppercase = require('./upperCase');
 
+test('verifica se passando uma string a transforma em letra maiúscula', (done) => {
+  uppercase('deivid', (str) => {
+    try {
+      expect(str).toBe('DEIVID');
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
