a23cb70f5182072d4e6086dbf7d5c9339aa4da8c
Deivid <deividmborges@gmail.com>
2021-10-05 20:32:44 -0300

    resolve exercicio 1

--- Trybe_exercicios/M2 - Desenvolvimento Front-end/B10 - Introdução ao React/exercicios-10-1/src/App.js
+++ Trybe_exercicios/M2 - Desenvolvimento Front-end/B10 - Introdução ao React/exercicios-10-1/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const myTasks = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>{ myTasks.map(task => Task(task)) }</ul>
     </div>
   );
 }
