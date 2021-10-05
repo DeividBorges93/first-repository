3c9616414d1684c6d7dd7ba2b990d1311b0c3e61
Deivid <deividmborges@gmail.com>
2021-10-05 20:38:48 -0300

    adiciona restante do exercicio 3

--- Trybe_exercicios/M2 - Desenvolvimento Front-end/B10 - Introdução ao React/exercicios-10-1-b/src/App.js
+++ Trybe_exercicios/M2 - Desenvolvimento Front-end/B10 - Introdução ao React/exercicios-10-1-b/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './Header';
 import Content from './Content';
+import Footer from './Footer';
 
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <div className="App">
       <Header />
       <Content />
-    </div>
+      <Footer />
+    </div>  
   );
 }
 
